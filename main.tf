9eebc7c8c14f43f14a48631e65b042862098f37e
David Celis <me@davidcel.is>
2018-10-26 10:47:00 -0700

    Add random_one

--- main.tf
+++ main.tf
@@ -6,6 +6,18 @@ resource "random_id" "random" {
   byte_length = 8
 }
 
+resource "random_pet" "random" {
+  keepers {
+    uuid = "${uuid()}"
+  }
+
+  length = 2
+}
+
 output "random" {
   value = "${random_id.random.hex}"
+}
+
+output "random_pet" {
+  value = "${random_pet.random.id}"
 }
